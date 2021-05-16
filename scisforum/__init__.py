917e8346fcd206e2aa11bfe064eab7b3950f143f
Nitin1901 <nitinsai.bommi@gmail.com>
2021-05-16 11:17:26 +0530

    normalized

--- scisforum/__init__.py
+++ scisforum/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '53ad147a12147000dc5eed019639856e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 from scisforum import routes
