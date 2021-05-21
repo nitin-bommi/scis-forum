17d987fdec2e50252ecd0fbb2bc8d89445239172
Nitin1901 <nitinsai.bommi@gmail.com>
2021-05-21 21:33:25 +0530

    .

--- scisforum/main/routes.py
+++ scisforum/main/routes.py
@@ -1,5 +1,4 @@
 from flask import render_template, request, Blueprint
-import random
 from scisforum.models import Post
 
 main = Blueprint('main', __name__)
@@ -10,8 +9,7 @@ main = Blueprint('main', __name__)
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
-    sidebar_posts = random.sample(Post.query.all(), 2)
-    return render_template('home.html', posts=posts, sidebar_posts=sidebar_posts)
+    return render_template('home.html', posts=posts)
 
 
 @main.route('/about')
