8176bcb2aeadb1351d9ad91db5cccceb4bfdee48
Amruta789 <amruta.jandhy@gmail.com>
2021-05-26 15:40:04 +0530

    Migrated database, previous records were deleted

--- scisforum/__init__.py
+++ scisforum/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from scisforum.config import Config
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
@@ -13,6 +14,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -22,6 +24,7 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app, db)
 
     from scisforum.users.routes import users
     from scisforum.posts.routes import posts
