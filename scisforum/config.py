81a7a4eb45eecf5d7f386412c9c0a908acf235c5
Nitin <nitinsai.bommi@gmail.com>
2021-05-28 03:58:24 +0000

    from server

--- scisforum/config.py
+++ scisforum/config.py
@@ -1,12 +1,17 @@
 import os
+import json
+
+
+with open('/etc/config.json') as config_file:
+	config = json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = '53ad147a12147000dc5eed019639856e'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///forum.db'
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD = config.get('MAIL_PASSWORD')
